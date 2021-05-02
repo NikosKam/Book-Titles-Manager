3c61dd8bedcbecb10d937bc7645f2f7bbba43d12
NikosKam <57060810+NikosKam@users.noreply.github.com>
2021-05-02 13:24:13 +0300

    Update book_titles.py

--- book_titles.py
+++ book_titles.py
@@ -1,11 +1,11 @@
 def main():
-    arr, li = [], []
-    file = open("/usercode/files/books.txt", "r")
-    lines = file.read().splitlines()
+    file = open("books.txt") #books.txt can be any txt file
+    #file = open("books.txt", "r") same thing
+    lines = file.read().splitlines() #this line removes all the '/n' at the end of each line so not to be counted in the l
     for line in lines:
-        f = line[0]
-        l = len(line)
-        print(f + str(l))
+        f = line[0] #the first letter of the the title
+        l = len(line) #the number of letters in the title
+        print(f + str(l)) #the coded name
 
     file.close()
     return
